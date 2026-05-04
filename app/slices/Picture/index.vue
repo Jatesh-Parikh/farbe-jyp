3065234d55c6eda49c47a5f1794519c40d64ed6f
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-04 22:49:31 +0530

    add picture slice

--- app/slices/Picture/index.vue
+++ app/slices/Picture/index.vue
@@ -6,16 +6,51 @@ defineProps(getSliceComponentProps<Content.PictureSlice>());
 
 <template>
   <section
-    :data-slice-type="slice.slice_type"
-    :data-slice-variation="slice.variation"
+    v-bind="getSceneAttributes({
+      position: 'top',
+      model: $prismic.isFilled.contentRelationship(slice.primary.product) ? slice.primary.product.uid : undefined
+    })"
+    class="grid xl:grid-cols-[3fr_2fr]"
   >
-    Placeholder component for picture (variation: {{ slice.variation }}) slices.
-
-    <br />
-    <strong>You can edit this slice directly in your code editor.</strong>
-    <!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
--->
+    <figure class="contents">
+      <PrismicImage 
+        :field="slice.primary.picture"
+        class="z-20 w-full row-span-2"
+        loading="lazy"
+      />
+      <SlideIn
+        as="figcaption"
+        class="px-4 pt-4 pb-16 rich-text"
+        :class="{
+          'xl:self-start': slice.variation === 'default' || slice.variation === 'bottom',
+          'xl:order-last xl:self-end': slice.variation === 'top',
+        }"
+      >
+        <PrismicRichText :field="slice.primary.caption" />
+      </SlideIn>
+    </figure>
+    <figure
+      v-if="$prismic.isFilled.image(slice.primary.secondary_picture)"
+      class="flex flex-col"
+      :class="{
+        'xl:self-end': slice.variation === 'default' || slice.variation === 'bottom',
+        'xl:slef-start': slice.variation === 'top', 
+      }"
+    >
+      <PrismicImage 
+        :field="slice.primary.secondary_picture"
+        class="z-20 w-full"
+        loading="lazy"
+      />
+      <SlideIn
+        as="figcaption"
+        class="px-4 pt-4 pb-16 rich-text"
+        :class="{
+          'xl:order-first': slice.variation === 'bottom'
+        }"
+      >
+        <PrismicRichText :field="slice.primary.secondary_caption" />
+      </SlideIn>
+    </figure>
   </section>
 </template>
