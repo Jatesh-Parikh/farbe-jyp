b1b660f50e6375713d97736c676074db9892ac60
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-04 21:29:50 +0530

    add dynamic page

--- app/pages/[uid].vue
+++ app/pages/[uid].vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
-import { asText } from '@prismicio/client'
-import { components } from '~/slices'
+  import { components } from '~/slices';
 
-const { client } = usePrismic()
-const route = useRoute()
-const { data: page } = await useAsyncData(route.params.uid as string, () =>
-  client.getByUID('page', route.params.uid as string)
-)
+  const prismic = usePrismic();
+  const route = useRoute();
+  const { data: page } = await useAsyncData(
+    `[page-uid-${route.params.uid}]`,
+    () => prismic.client.getByUID("page", route.params.uid as string),
+  )
+  const { data: stripeProducts } = await useFetch("/api/products");
 
-useHead({
-  title: asText(page.value?.data.title)
-})
+  useSeoMeta({
+    title: page.value?.data.meta_title ?? undefined,
+    ogTitle: page.value?.data.meta_title ?? undefined,
+    description: page.value?.data.meta_description ?? undefined,
+    ogDescription: page.value?.data.meta_description ?? undefined,
+    ogImage: computed(() => prismic.asImageSrc(page.value?.data.meta_image) ?? undefined)
+  });
+
 </script>
-
 
 <template>
   <main>
-    <SliceZone :slices="page?.data.slices ?? []" :components="components" />
+    <SliceZone 
+      :slices="page?.data.slices ?? []" 
+      :components="components" 
+      :context="{ stripeProducts }"  
+    />
   </main>
 </template>
