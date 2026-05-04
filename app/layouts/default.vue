a5f71107f89543c9f258f26a37fcff34ae4d640c
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-04 21:21:21 +0530

    add default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useCart } from '~/composables/useCart';
+
+const route = useRoute();
+const prismic = usePrismic();
+
+const { data: settings } = await useAsyncData("settings", () => prismic.client.getSingle("settings"));
+
+useSeoMeta({
+    title: settings.value?.data.site_title,
+    ogTitle: settings.value?.data.site_title,
+    description: settings.value?.data.meta_description,
+    ogDescription: settings.value?.data.meta_description,
+    ogImage: () => prismic.asImageSrc(settings.value?.data.meta_image),
+});
+
+onMounted(() => {
+    if (route.query.order === "completed") {
+        useCart().clear();
+        useRouter().replace({ path: route.path })
+    }
+})
+</script>
+
+<template>
+    <div>
+        <AppHeader :settings="settings" class="fixed top-0 left-0 right-0 z-10" />
+        <slot />
+        <AppFooter :settings="settings" />
+        <TCanvas class="fixed top-0 left-0 right-0 h-lvh -z-1">
+            <TScene />
+        </TCanvas>
+    </div>
+</template>
+
